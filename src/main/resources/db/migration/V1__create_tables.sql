9798a11ded259ff05d8517d0e72e9f8f6bf60c8f
e.amanturov <edi.amanturov@gmail.com>
2024-06-18 11:40:16 +0600

    check migration

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,31 +1,12 @@
-CREATE TABLE jwt_tokens (
-                            id SERIAL PRIMARY KEY,
-                            token VARCHAR(255) NOT NULL,
-                            expiry_date TIMESTAMP NOT NULL,
-                            user_id INT,
-                            FOREIGN KEY (user_id) REFERENCES users (id)
-);
-
-CREATE TABLE attachments (
-                             id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                             path VARCHAR(255) NOT NULL,
-                             type VARCHAR(255) NOT NULL,
-                             extension VARCHAR(255) NOT NULL,
-                             name VARCHAR(255) NOT NULL,
-                             description TEXT,
-                             user_id BIGINT,
-                             CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL
-);
-
 
 CREATE TABLE common_reference_type (
-                                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                       id  SERIAL PRIMARY KEY,
                                        title VARCHAR(255) NOT NULL,
                                        code VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE common_reference (
-                                  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                  id SERIAL PRIMARY KEY,
                                   title VARCHAR(255) NOT NULL,
                                   code VARCHAR(255) NOT NULL,
                                   type_id BIGINT,
@@ -35,7 +16,7 @@ CREATE TABLE common_reference (
 );
 
 CREATE TABLE users (
-                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                       id SERIAL PRIMARY KEY,
                        role VARCHAR(255) NOT NULL,
                        username VARCHAR(255) NOT NULL,
                        password VARCHAR(255) NOT NULL,
@@ -50,4 +31,26 @@ CREATE TABLE users (
                        CONSTRAINT fk_region FOREIGN KEY (region_id) REFERENCES common_reference(id) ON DELETE SET NULL,
                        CONSTRAINT fk_district FOREIGN KEY (district_id) REFERENCES common_reference(id) ON DELETE SET NULL
 );
+
+
+CREATE TABLE jwt_tokens (
+                            id SERIAL PRIMARY KEY,
+                            token VARCHAR(255) NOT NULL,
+                            expiry_date TIMESTAMP NOT NULL,
+                            user_id INT,
+                            FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE attachments (
+                             id  SERIAL PRIMARY KEY,
+                             path VARCHAR(255) NOT NULL,
+                             type VARCHAR(255) NOT NULL,
+                             extension VARCHAR(255) NOT NULL,
+                             name VARCHAR(255) NOT NULL,
+                             description TEXT,
+                             user_id BIGINT,
+                             CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL
+);
+
+
 
